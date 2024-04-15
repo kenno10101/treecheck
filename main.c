5f66c81df4cba4c6f96ce406dd04dfd2d6778caf
Kenn-Michael Sanga <kennsanga@gmail.com>
2024-04-16 00:36:04 +0200

    part 1 done (print all stats)

--- main.c
+++ main.c
@@ -8,6 +8,25 @@ typedef struct tnode
     struct tnode *right;
 } tnode;
 
+typedef struct node
+{
+    int value;
+    struct node *next;
+} node;
+
+node *addNode(node *head, int value)
+{
+    node *newNode = (node *)malloc(sizeof(node));
+    newNode->value = value;
+    newNode->next = NULL;
+    if (head == NULL)
+    {
+        return newNode;
+    }
+    newNode->next = head;
+    return newNode;
+}
+
 tnode *insertValue(tnode *root, int value)
 {
     // if root is null, create a new node (base case)
@@ -81,30 +100,70 @@ int bal(tnode *root)
 }
 
 // postorder (right then left), calculate balance, store value for min/max/avg later
-void postOrder(tnode *root)
+void postOrder(tnode *root, node **head, int *avlViolation)
 {
     if (root == NULL)
     {
         return;
     }
-    postOrder(root->right);
-    postOrder(root->left);
+    postOrder(root->right, head, avlViolation);
+    postOrder(root->left, head, avlViolation);
     printf("bal(%d)=%d", root->key, bal(root));
     if (bal(root) > 1 || bal(root) < -1)
     {
+        *avlViolation = 1;
         printf(" (AVL violation!)");
     }
     printf("\n");
     // add number to list for min/max/avg
+    *head = addNode(*head, root->key);
+}
+
+void printStats(node *head)
+{
+    int min = head->value, max = head->value;
+    float counter = 0, sum = 0;
+
+    if (head == NULL)
+    {
+        return;
+    }
+    while (head != NULL)
+    {
+        sum += head->value;
+        counter++;
+        if (head->value < min)
+        {
+            min = head->value;
+        }
+        if (head->value > max)
+        {
+            max = head->value;
+        }
+        head = head->next;
+    }
+    float avg = sum / counter;
+    printf("min: %d, max: %d, avg: %.1f\n", min, max, avg);
 }
 
 int main()
 {
 
     tnode *root = NULL;
+    node *head = NULL;
+    int avlViolation = 0;
     char *filename = "input.txt";
     root = inputFile(filename, root);
-    postOrder(root);
+    postOrder(root, &head, &avlViolation);
+    if (avlViolation == 0)
+    {
+        printf("AVL: yes\n");
+    }
+    else
+    {
+        printf("AVL: no\n");
+    }
+    printStats(head);
 
     // int depth = maxDepth(root);
     // printf("depth of root: %d\n", depth);
